5e3537c3cadc460f0566f21e8109c35afe840e7f
Daniel Ortega <83851986+rekirem@users.noreply.github.com>
2022-05-23 17:42:38 -0400

    Update ReverseAString.js

--- ReverseAString.js
+++ ReverseAString.js
@@ -5,11 +5,3 @@ function invertString(str) {
     }
     return reverse;
   }
-
-  //Another way to do it
-    /*
-    return str.split('').reverse().join('');
-    split transforma un  string en un array
-    reverse invierte el orden de los elementos del array
-    join convierte los elementos de un array en un string
-    */
