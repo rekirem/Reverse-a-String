d4aee436b556d2bc6341d9e9cf84e902281a5333
Daniel Ortega <83851986+rekirem@users.noreply.github.com>
2022-05-23 17:42:14 -0400

    Create anotherWay.js

---
+++ anotherWay.js
@@ -0,0 +1,8 @@
+function reverseString(str) {
+    return str.split('').reverse().join('');
+  }
+    /*
+    split transforma un  string en un array
+    reverse invierte el orden de los elementos del array
+    join convierte los elementos de un array en un string
+    */
